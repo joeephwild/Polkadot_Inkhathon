a9ae31276cf538854888519f03bb9fd16c9d531e
joeephwild <josephomotade0@gmail.com>
2024-03-06 17:22:13 +0100

    commit contract

--- polkadot_contract/lib.rs
+++ polkadot_contract/lib.rs
@@ -1,69 +1,115 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 #[ink::contract]
-pub mod polkadot_contract {
+mod store_contract {
+    use ink::prelude::string::String;
+    use ink::prelude::vec::Vec;
+    use ink::prelude::collections::BTreeMap;
+    use ink::storage::Mapping;
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+    #[derive(Debug, Clone, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub struct App {
+        uri: String,
+        owner: AccountId,
+        price: Balance,
+    }
+
     #[ink(storage)]
-    pub struct PolkadotContract {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+    pub struct StoreContract {
+        all_app: Vec<App>,
+        address_app_count: Mapping<AccountId, u64>,
+        id_to_app: BTreeMap<u64, App>,
+        app_count: u128,
+        address_to_app: Mapping<AccountId, Vec<App>>,
     }
 
-    impl PolkadotContract {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
+    impl StoreContract {
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new() -> Self {
+            Self {
+                all_app: Vec::new(),
+                address_app_count: Mapping::new(),
+                id_to_app: BTreeMap::new(),
+                app_count: 0,
+                address_to_app: Mapping::new()
+            }
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn upload_an_app(&mut self, app_uri: String, amount: Balance) {
+           
         }
 
-        /// Simply returns the current value of our `bool`.
+        #[ink(message, payable)]
+        pub fn install_app(&mut self, app_id: u64) {
+            let caller = self.env().caller();
+            let app = self.id_to_app.get(&app_id).expect("App does not exist");
+            assert!(self.env().transferred_value() >= app.price, "Insufficient Funds");
+            assert_ne!(caller, app.owner, "Owner can't download");
+
+            if self.env().transfer(app.owner, self.env().transferred_value()).is_err() {
+                panic!("error transferring")
+            }
+
+            self.env().emit_event(AppInstalled {
+                app_id,
+                buyer: caller,
+                owner: app.owner,
+                price: app.price,
+            });
+        }
+
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
+        pub fn get_app(&self, app_id: u64) -> Option<App> {
+            self.id_to_app.get(&app_id).cloned()
+        }
+
+        #[ink(message)]
+        pub fn get_all_apps(&self) -> Vec<App> {
+            self.all_app.clone()
+        }
+
+        // #[ink(message)]
+        // pub fn get_owner_app_count(&self, owner: AccountId) -> u64 {
+        //     self.address_app_count.get(&owner).copied().unwrap_or(0)
+        // }
+
+        #[ink(message)]
+        pub fn get_all_app_uris(&self) -> Vec<String> {
+            self.all_app.iter().map(|app| app.uri.clone()).collect()
+        }
+
+        #[ink(message)]
+        pub fn get_user_app_uri(&self, user: AccountId, index: u64) -> Option<String> {
+            let mut count = 0;
+            for app in &self.all_app {
+                if app.owner == user {
+                    if count == index {
+                        return Some(app.uri.clone());
+                    }
+                    count += 1;
+                }
+            }
+            None
         }
     }
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
+    #[ink(event)]
+    pub struct AppUploaded {
+        #[ink(topic)]
+        app_id: u64,
+        uri: String,
+        owner: AccountId,
+        price: Balance,
+    }
 
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let polkadot_contract = PolkadotContract::default();
-            assert_eq!(polkadot_contract.get(), false);
-        }
-
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut polkadot_contract = PolkadotContract::new(false);
-            assert_eq!(polkadot_contract.get(), false);
-            polkadot_contract.flip();
-            assert_eq!(polkadot_contract.get(), true);
-        }
+    #[ink(event)]
+    pub struct AppInstalled {
+        #[ink(topic)]
+        app_id: u64,
+        buyer: AccountId,
+        owner: AccountId,
+        price: Balance,
     }
 }
