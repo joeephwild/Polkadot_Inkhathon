137db6e767037581a8c9f5f5f1a36b9f9e84cfb3
joeephwild <josephomotade0@gmail.com>
2024-03-06 18:16:27 +0100

    store contract done

--- polkadot_contract/lib.rs
+++ polkadot_contract/lib.rs
@@ -13,6 +13,7 @@ mod store_contract {
         uri: String,
         owner: AccountId,
         price: Balance,
+        app_id: u128,
     }
 
     #[ink(storage)]
@@ -21,7 +22,25 @@ mod store_contract {
         address_app_count: Mapping<AccountId, u64>,
         id_to_app: BTreeMap<u64, App>,
         app_count: u128,
-        address_to_app: Mapping<AccountId, Vec<App>>,
+        address_to_app: BTreeMap<AccountId, Vec<App>>,
+    }
+
+    #[ink(event)]
+    pub struct AppUploaded {
+        #[ink(topic)]
+        app_id: u64,
+        uri: String,
+        owner: AccountId,
+        price: Balance,
+    }
+
+    #[ink(event)]
+    pub struct AppInstalled {
+        #[ink(topic)]
+        app_id: u64,
+        buyer: AccountId,
+        owner: AccountId,
+        price: Balance,
     }
 
     impl StoreContract {
@@ -32,22 +51,37 @@ mod store_contract {
                 address_app_count: Mapping::new(),
                 id_to_app: BTreeMap::new(),
                 app_count: 0,
-                address_to_app: Mapping::new()
+                address_to_app: BTreeMap::new()
             }
         }
 
         #[ink(message)]
-        pub fn upload_an_app(&mut self, app_uri: String, amount: Balance) {
-           
+        pub fn upload_an_app(&mut self, _app_uri: String, _amount: Balance) -> u128 {
+            let caller: ink::primitives::AccountId = self.env().caller();
+            let mut count: u128 = self.app_count;
+
+            // Create a new App instance
+            let new_app: App = App {
+                uri: _app_uri.clone(),
+                owner: caller,
+                price: _amount,
+                app_id: count
+            };
+              
+              // push new app to the all_app vec
+            self.all_app.push(new_app.clone());
+            
+            count += 1;
+            count
         }
 
         #[ink(message, payable)]
         pub fn install_app(&mut self, app_id: u64) {
-            let caller = self.env().caller();
-            let app = self.id_to_app.get(&app_id).expect("App does not exist");
+            let caller: ink::primitives::AccountId = self.env().caller();
+            let app: &App = self.id_to_app.get(&app_id).expect("App does not exist");
             assert!(self.env().transferred_value() >= app.price, "Insufficient Funds");
             assert_ne!(caller, app.owner, "Owner can't download");
-
+            
             if self.env().transfer(app.owner, self.env().transferred_value()).is_err() {
                 panic!("error transferring")
             }
@@ -70,14 +104,9 @@ mod store_contract {
             self.all_app.clone()
         }
 
-        // #[ink(message)]
-        // pub fn get_owner_app_count(&self, owner: AccountId) -> u64 {
-        //     self.address_app_count.get(&owner).copied().unwrap_or(0)
-        // }
-
         #[ink(message)]
         pub fn get_all_app_uris(&self) -> Vec<String> {
-            self.all_app.iter().map(|app| app.uri.clone()).collect()
+            self.all_app.iter().map(|app: &App| app.uri.clone()).collect()
         }
 
         #[ink(message)]
@@ -95,21 +124,5 @@ mod store_contract {
         }
     }
 
-    #[ink(event)]
-    pub struct AppUploaded {
-        #[ink(topic)]
-        app_id: u64,
-        uri: String,
-        owner: AccountId,
-        price: Balance,
-    }
-
-    #[ink(event)]
-    pub struct AppInstalled {
-        #[ink(topic)]
-        app_id: u64,
-        buyer: AccountId,
-        owner: AccountId,
-        price: Balance,
-    }
+  
 }
