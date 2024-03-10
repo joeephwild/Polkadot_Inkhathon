ef9a1664ded08e8a0eb1a6f1196bef7f1faff38c
joeephwild <josephomotade0@gmail.com>
2024-03-10 15:19:21 +0100

    COMMIT

--- polkadot_contract/src/pack/lib.rs
+++ polkadot_contract/src/pack/lib.rs
@@ -2,12 +2,32 @@
 
 #[ink::contract]
 mod pack_contract {
-    use std::process::id;
-
     use ink::prelude::string::String;
-    use ink::prelude::vec::Vec;
     use ink::prelude::collections::BTreeMap;
     use ink::storage::Mapping;
+    use ink::contract_ref;
+    use ink::{
+        prelude::vec::Vec,
+    };
+    use psp34::{Id, PSP34};
+
+    #[ink::trait_definition]
+    pub trait PSP34Mintable {
+    /// Mints a token to the sender's account.
+    ///
+    /// # Events
+    ///
+    /// On success a `Transfer` event is emitted with `None` sender.
+    ///
+    /// # Errors
+    ///
+    /// Reverts with `TokenExists`` if token id is already in the library.
+    /// 
+    /// Reverts with `Custom (max supply exceeded)` if the incremented by 1 total
+    /// supply exceeds maximal value of `u128` type.
+    #[ink(message)]
+    fn mint(&mut self, id: Id) -> Result<()>;
+   }
 
     #[derive(Debug, Clone, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
@@ -16,7 +36,8 @@ mod pack_contract {
        pack_namme: String,
        pack_desc: String,
        quanity: u128,
-       price: u128
+       price: u128,
+       owner: AccountId
     }
  
     #[ink(storage)]
@@ -30,9 +51,9 @@ mod pack_contract {
 
     #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
-  pub enum Error {
-    FailedToInsert,
-  }  
+    pub enum Error {
+      FailedToInsert,
+    }  
 
     #[ink(event)]
     pub struct Pacs {}
@@ -48,28 +69,44 @@ mod pack_contract {
         }
 
         #[ink(message)]
-        pub fn create_pack(&mut self, _pack_address: AccountId, desc: String, _name: String, amount: u128, _quanity: u128)  -> Result<()> {
-            let caller: AccountId = self.env().caller();
-            let pack: Option<PackContract> = self.address_to_pack.insert(caller, PackContract{
+        pub fn create_pack(&mut self, _pack_address: AccountId, desc: String, _name: String, amount: u128, _quanity: u128, _receiver: AccountId)  {
+            let _caller: AccountId = self.env().caller();
+            let pack: Option<PackContract> = self.address_to_pack.insert(_caller, PackContract{
                 game_address: _pack_address,
                 pack_desc: desc,
                 pack_namme: _name,
                 price: amount,
-                quanity: _quanity
+                quanity: _quanity,
+                owner: _receiver
             });
             if let Some(pack_to_push) = pack {
                 self.all_pack.push(pack_to_push);
-                return  Ok(());
-            } else {
-                // Handle the case where the pack was not inserted, if necessary
-                return Err(Error::FailedToInsert)
             }
         }
 
         #[ink(message, payable)]
-        pub fn buy_pack(&self, _quantity: u128, _id: u128) {
-            let caller: ink::primitives::AccountId = self.env().caller();
+        pub fn buy_pack(&self, _quantity: u128, _id: u128) -> u128 {
+            let _caller: AccountId = self.env().caller();
+            let money_sent: u128 = self.env().transferred_value();
+            let pack: &PackContract = self.id_to_pack.get(&_id).expect("App does not exist");
+            assert!(money_sent >= pack.price, "InsufficientFunds");
 
+            let mut game_pack: contract_ref!(PSP34Mintable) = pack.game_address.into();
+
+            // send require money to the address
+            if self.env().transfer(pack.owner, money_sent).is_err() {
+                panic!("error transferring")
+            }
+
+            // game_pack.mint(_id);
+
+           let remianing_quantity: u128 = pack.quanity - _quantity;
+           remianing_quantity
+        }
+
+        #[ink(message)]
+        pub fn retrive_all_pack(&mut self) -> Vec<PackContract> {
+            return self.all_pack.clone();
         }
     }
 }
